1c9026c375bf4373868d984feddeaf492ec02136
Jozef Lang <developer@jozeflang.com>
2017-03-11 21:41:08 +0100

    Iter #4

--- ebill-client/app/controllers/BillsCtrl.js
+++ ebill-client/app/controllers/BillsCtrl.js
@@ -11,16 +11,38 @@ myApp.controller('BillsCtrl', ['$rootScope', '$scope', '$location', 'LoginServic
   };
 
   $scope.payBill = function(bill) {
-    bill.status = 'paid';
+    if (isReadOnly(bill)) {
+      console.log('Bill ' + bill.uuid + ' is read only!');
+    }
+    
+    $http({
+      method: 'POST',
+      url: 'api/ebill/' + bill.uuid +'/pay',
+    }).then(function success(response) {
+      console.log('Ebill ' + bill.uuid + ' paid');
+    }); 
   };
 
   $scope.rejectBill = function(bill) {
-    bill.status = 'rejected';
+    if (isReadOnly(bill)) {
+      console.log('Bill ' + bill.uuid + ' is read only!');
+    }
+
+    $http({
+      method: 'POST',
+      url: 'api/ebill/' + bill.uuid +'/reject',
+    }).then(function success(response) {
+      console.log('Ebill ' + bill.uuid + ' reject');
+    }); 
   };
 
   $scope.statusStyle = function(bill) {
     return {'open': 'color_orange', 'paid': 'color_green', 'rejected': 'color_red'}[bill.status];
   };
+
+  $scope.isReadOnly = function(bill) {
+    return bill.status !== 'open';
+  }
 
 
   $scope.loadData();
