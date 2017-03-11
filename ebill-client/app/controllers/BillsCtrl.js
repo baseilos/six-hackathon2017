c3a2ef4c5a07ae00b9f21454b4d183df48e8f386
Jozef Lang <developer@jozeflang.com>
2017-03-11 20:48:52 +0100

    Sourcing bills from the server

--- ebill-client/app/controllers/BillsCtrl.js
+++ ebill-client/app/controllers/BillsCtrl.js
@@ -1,40 +1,9 @@
 myApp.controller('BillsCtrl', ['$rootScope', '$scope', '$location', 'LoginService', '$http', function ($rootScope, $scope, $location, LoginService, $http) {
 
-  $scope.bills = [{
-    issuer: 'My Poster GMBH',
-    currency: 'CHF',
-    amount: '30',
-    status: 'open'
-  }, 
-  {
-    issuer: 'Dentist Dr. med. Mueller',
-    currency: 'CHF',
-    amount: '1198',
-    status: 'open'
-  },
-  {
-    issuer: 'Car services',
-    currency: 'CHF',
-    amount: '270',
-    status: 'open'
-  },
-  {
-    issuer: 'Taxes',
-    currency: 'CHF',
-    amount: '13000',
-    status: 'open'
-  },
-  {
-    issuer: 'MasterCard',
-    currency: 'CHF',
-    amount: '1300',
-    status: 'open'
-  }];
-
   $scope.loadData = function() {
     $http({
       method: 'GET',
-      url: 'http://172.30.5.72:3000/ebill',
+      url: 'api/ebill',
     }).then(function success(response) {
       console.log('Ebill data loaded: ' + response);
         $rootScope.bills = response; 
